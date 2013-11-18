7e7d7cd4cde77e4e0755ddcb59f101136186246d
Cal Paterson <cpaterso@thoughtworks.com>
2013-11-18 14:00:51 +0000

    Move setup.py out to root of repo

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ from setuptools import setup, find_packages
 setup(
     author="INSS",
     author_email="cpaterso@thoughtworks.com",
-    name="redundancy_payments_service",
-    packages=find_packages(),
-    scripts=["redundancy_payments_service"],
+    name="redundancy_payments_alpha",
+    packages=find_packages("redundancy_payments_alpha"),
+    package_dir = {'':'redundancy_payments_alpha'},
+    scripts=["redundancy_payments_alpha/redundancy_payments_service"],
     version=os.environ.get("BUILD_NUMBER", "dev"),
     install_requires=[str(req.req) for req in
-                      parse_requirements("requirements.dev.txt")],
+                      parse_requirements("redundancy_payments_alpha/requirements.dev.txt")],
     include_package_data=True,
     )
