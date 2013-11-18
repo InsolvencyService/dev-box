741d893b0a76740578b766ac7e0ca925d81efdda
Cal Paterson <cpaterso@thoughtworks.com>
2013-11-18 16:59:03 +0000

    Add ensure_clean_tables as a script to setuptools

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     name="redundancy_payments_alpha",
     packages=find_packages("redundancy_payments_alpha"),
     package_dir = {'':'redundancy_payments_alpha'},
-    scripts=["redundancy_payments_alpha/redundancy_payments_service"],
+    scripts=[
+        "redundancy_payments_alpha/redundancy_payments_service",
+        "redundancy_payments_alpha/ensure_clean_tables"
+    ],
     version=os.environ.get("BUILD_NUMBER", "dev"),
     install_requires=[str(req.req) for req in
                       parse_requirements("redundancy_payments_alpha/requirements.dev.txt")],
