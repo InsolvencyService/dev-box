5cb0580231f1a920d0723e97bbf9bd40269c6cc0
Cal Paterson <cpaterso@thoughtworks.com>
2013-11-18 17:10:37 +0000

    Add insolvency_practitioner_app to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     package_dir = {'':'redundancy_payments_alpha'},
     scripts=[
         "redundancy_payments_alpha/redundancy_payments_service",
-        "redundancy_payments_alpha/ensure_clean_tables"
+        "redundancy_payments_alpha/insolvency_practitioner_app",
+        "redundancy_payments_alpha/ensure_clean_tables",
     ],
     version=os.environ.get("BUILD_NUMBER", "dev"),
     install_requires=[str(req.req) for req in
