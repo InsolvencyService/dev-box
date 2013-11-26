4598f12344f9f6d201707c26e77dfdd4fc3cd00c
Gareth <grogers@thoughtworks.com>
2013-11-26 10:34:39 +0000

    Move fake email service out of api

--- redundancy_payments_alpha/notification_service/api.py
+++ redundancy_payments_alpha/notification_service/api.py
@@ -1,5 +1,5 @@
 import os
-from email import _send_email_via_mandrill
+from email import _send_email_via_mandrill, _write_email_to_disk
 
 
 def send_email(to_email, subject, to_name, message_text):
@@ -9,14 +9,15 @@ def send_email(to_email, subject, to_name, message_text):
 
     use_mock_email = os.environ.get('USE_MOCK_EMAIL', None)
 
-    if(use_mock_email):
-        with open('TestEmail.txt', 'w') as f:
-            f.write('From Email: %s' % from_email)
-            f.write('From Name: %s' % from_name)
-            f.write('To Address: %s' % to_email)
-            f.write('Subject: %s' % subject)
-            f.write('To Name: %s' % to_name)
-            f.write('Message Text: %s' % message_text)
+    if use_mock_email:
+        _write_email_to_disk(
+            to_email=to_email,
+            to_name=to_name,
+            from_email=from_email,
+            from_name=from_name,
+            subject=subject,
+            text=message_text
+        )
     else:
         _send_email_via_mandrill(
             to_email=to_email,
@@ -26,4 +27,3 @@ def send_email(to_email, subject, to_name, message_text):
             subject=subject,
             text=message_text
         )
-
--- redundancy_payments_alpha/notification_service/email.py
+++ redundancy_payments_alpha/notification_service/email.py
@@ -1,8 +1,11 @@
+import json
+import os
+
 import mandrill
 from config import API_KEY
 
-def _send_email_via_mandrill(to_email, to_name, from_email, from_name, subject, text):
 
+def _send_email_via_mandrill(to_email, to_name, from_email, from_name, subject, text):
     mandrill_api = mandrill.Mandrill(API_KEY)
     message = {
         'auto_text': True,
@@ -21,3 +24,19 @@ def _send_email_via_mandrill(to_email, to_name, from_email, from_name, subject, 
         ip_pool='Main Pool'
     )
 
+
+def _write_email_to_disk(to_email, to_name, from_email, from_name, subject, text):
+    output = os.path.join(os.getcwd(), 'sent_emails', to_email)
+    
+    with open(output, 'w') as f:
+        email = {
+            'to_email': to_email,
+            'to_name': to_name,
+            'subject': subject,
+            'text': text,
+            'from_email': from_email,
+            'from_name': from_name
+        }
+        f.write(json.dumps(email))
+
+    print 'Fake Mail Service: to %s' % to_email
